b60c2ea461e379c5ffa78c112f0b50cd6abcffad
GavristovDenis <102148379+GavristovDenis@users.noreply.github.com>
2023-06-27 03:40:53 +0300

    main functionality added

--- src/App.tsx
+++ src/App.tsx
@@ -3,18 +3,41 @@ import { ToDoList, ToDoListProps } from "./components/ToDoList";
 const App = () => {
   const [toDoArray, setToDoArray] = useState([
     {
-      name: "Помыть деда",
+      name: "Купить молоко",
     },
   ]);
   const [inputValue, setInputValue] = useState("");
-
+  const [editMode, setEditMode] = useState(false);
+  const [editText, setEditText] = useState("");
+  const [foundIndex, setFoundIndex] = useState(0);
   function deleteToDo(index: number) {
     const filteredArray = toDoArray.filter((item, i) => i !== index);
     setToDoArray(filteredArray);
   }
+  function turnEditModeOn(index: number) {
+    setEditMode(true);
+    setFoundIndex(index);
+  }
+
+  function editToDo() {
+    const newArray = [...toDoArray];
+    newArray.splice(foundIndex, 1, { name: editText });
+    setToDoArray(newArray);
+    setEditMode(false);
+    setEditText("");
+  }
 
   return (
     <div className="App">
+      {editMode ? (
+        <div className="editModal">
+          <div className="modalContent">
+            <input onChange={(e) => setEditText(e.target.value)}></input>
+            <button onClick={() => editToDo()}>Готово!</button>
+          </div>
+        </div>
+      ) : null}
+
       <input onChange={(e) => setInputValue(e.target.value)}></input>
       <button
         onClick={() =>
@@ -30,6 +53,7 @@ const App = () => {
             name={toDo.name}
             number={index + 1}
             onDelete={() => deleteToDo(index)}
+            onEdit={() => turnEditModeOn(index)}
           />
         );
       })}
--- src/components/ToDoList.tsx
+++ src/components/ToDoList.tsx
@@ -3,16 +3,23 @@ import React, { FC } from "react";
 export interface ToDoListProps {
   name: string;
   number: number;
-  onDelete: () => void;
+  onDelete?: () => void;
+  onEdit: () => void;
 }
 
-export const ToDoList: FC<ToDoListProps> = ({ name, number, onDelete }) => {
+export const ToDoList: FC<ToDoListProps> = ({
+  name,
+  number,
+  onDelete,
+  onEdit,
+}) => {
   return (
     <div className="toDoList">
       <div>
         {number}. {name}
       </div>
       <button onClick={onDelete}>Удалить</button>
+      <button onClick={onEdit}>Редактировать</button>
     </div>
   );
 };
